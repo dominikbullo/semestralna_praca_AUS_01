842ae5e5be0cd6bd8a9448f64252b43d8f3b56f5
Dominik <dominobullo@gmail.com>
2019-03-23 12:47:34 +0100

    zaloha

--- SemestralnaPraca_AUS_01/main.cpp
+++ SemestralnaPraca_AUS_01/main.cpp
@@ -5,8 +5,9 @@
 int main()
 {
 	// TODO 23.03 -> pridať pole regionov
-	// TODO 23.03 -> pridať autu pole regionov ako trasu ktorou pôjde
+	// FIXED 23.03 -> pridať autu pole regionov ako trasu ktorou pôjde
 
+	// ČAS
 	// TODO 23.03 -> meneźovanie času, kontrola dronu, či stihne vyzdvihnuť zasielku
 	// TODO 23.03 -> uloženie vozidla do Arraylistu podľa dátumu
 
@@ -15,9 +16,12 @@ int main()
 	// TODO 23.03 ->!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	// TODO 23.03 -> či naloženie zásielky neprekročí hmotnosť auta
-	// TODO 23.03 -> dron musí byť voľný;
+		// prejdem si všetky auta, najdem ktore ide cez ten kraj a zistím či neprekroci hmotnost
+
+	// TODO 23.03 -> dron musí byť voľný; -> easy
 	// TODO 23.03 -> ak viac dronov spĺňa vyššie špecifikované pravidlá, prioritne sa použije dron s najmenšou nosnosťou, ktorá ešte umožní prepraviť danú zásielku;
 	// TODO 23.03 -> ak viac dronov spĺňa vyššie špecifikované pravidlá, vyberie sa ten, ktorý je aktuálne najviac nabitý.
+		// zoradenie priamo v arrayliste ?
 
 	// TODO 24.03 -> 7 - 8 - 11 -> ukladanie ho súbora ?
 
